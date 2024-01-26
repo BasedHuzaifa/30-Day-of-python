c53327099acaf288d23227906e3c5349c2f58bda
BasedHuzaifa <134192592+BasedHuzaifa@users.noreply.github.com>
2024-01-26 19:58:33 +0500

    Delete new-exercise/ex.py

--- new-exercise/ex.py
+++
@@ -1,7 +0,0 @@
-import math
-point1 = (2, 3)
-point2 = (10, 8)
-x1, y1 = point1
-x2, y2 = point2
-distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-print(f"The Euclidean distance between {point1} and {point2} is: {distance}")
